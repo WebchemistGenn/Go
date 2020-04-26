5b085d1d5f9033f10b20d6900266399e6613ff6b
WebchemistGenn <yoonwonyoul@webchemist.net>
2020-04-26 21:40:16 +0900

    change some

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/WebchemistGenn/go/something"
 )
 
 func main() {
 	const message string = "Hello"
 	fmt.Println("test")
-	something.Say(message)
+
 }
