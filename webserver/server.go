bd19aec4b1da38e73ebe8a56cba7b3188faec6b0
WebchemistGenn <yoonwonyoul@webchemist.net>
2020-06-28 15:38:02 +0900

    basic

---
+++ webserver/server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	http.ListenAndServe(":3000", mux)
+}
