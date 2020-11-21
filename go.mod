5016f685359007b6362fbf169a3fb457b781750c
WebchemistGenn <yoonwonyoul@webchemist.net>
2020-11-22 01:09:13 +0900

    change code

--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/WebchemistGenn/go
-
-go 1.14
-
-require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
-	github.com/gorilla/mux v1.7.4
-)
--- something/something.go
+++
@@ -1,9 +0,0 @@
-package something
-
-import "fmt"
-
-// Say is an example function
-func Say(message string, test string) {
-	fmt.Println(message)
-	fmt.Println(test)
-}
